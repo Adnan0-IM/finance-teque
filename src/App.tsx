19302a3ecb9390ddd3f1946ca095cb6781833986
Adnan0-IM <adnanilyas969@gmail.com>
2025-08-13 11:15:18 +0100

    added investment and shariah

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router";
 import HomePage from "./pages/HomePage";
 import { AboutPage } from "./pages/AboutPage";
-// import { InvestmentPlansPage } from "./pages/InvestmentPlansPage";
-// import { ShariahCompliancePage } from "./pages/ShariahCompliancePage";
+import { InvestmentPlansPage } from "./pages/InvestmentPlansPage";
+import { ShariahCompliancePage } from "./pages/ShariahCompliancePage";
 import { ContactPage } from "./pages/ContactPage";
 import { TeamPage } from "./pages/TeamPage";
 
@@ -12,8 +12,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
-        {/* <Route path="/plans" element={<InvestmentPlansPage />} />
-        <Route path="/shariah" element={<ShariahCompliancePage />} /> */}
+        <Route path="/plans" element={<InvestmentPlansPage />} />
+        <Route path="/shariah" element={<ShariahCompliancePage />} />
         <Route path="/team" element={<TeamPage />} />
         <Route path="/contact" element={<ContactPage />} />
         <Route path="*" element={<NotFoundPage />} />
